f65a94c87157095cae237d68e1b03b0fecd86dcd
Evelyn <135255271+evygtk@users.noreply.github.com>
2024-09-20 13:24:34 -0300

    h

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 window.onload = function() {
     console.log("Welcome to my portfolio site!");
 };
+<><h1 id="titulo">Clique em mim</h1><script> document.getElementById("titulo").addEventListener("click", function() {alert("Você clicou no título!")}; {"}"}
+); </script></>
